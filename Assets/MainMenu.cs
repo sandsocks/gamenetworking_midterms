470ceb324b2de53566b7a88adb15bb5f0bd8985f
sandsocks <max.sandejas@gmail.com>
2026-02-22 21:38:36 +0800

    empty for the tutorial panel

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+     public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
